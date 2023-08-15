6875c0ef015650ca987b8615d4512068d6480c11
Ravana ⭐ <47528708+ravana69@users.noreply.github.com>
2023-08-15 22:09:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Math.random Noise
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/sfi0zy/pen/QGwwJo](https://codepen.io/sfi0zy/pen/QGwwJo).
+
---
+++ dist/script.js
@@ -0,0 +1,29 @@
+let canvas = document.getElementById('noisy-canvas'),
+context = canvas.getContext('2d');
+
+let getRandom = () => {
+  return Math.random() * 255;
+};
+
+let render = () => {
+  const height = window.innerHeight,
+  width = window.innerWidth;
+
+  context.canvas.height = height;
+  context.canvas.width = width;
+
+  let imageData = context.createImageData(width, height);
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    imageData.data[i] = getRandom();
+    imageData.data[i + 1] = getRandom();
+    imageData.data[i + 2] = getRandom();
+    imageData.data[i + 3] = 255;
+  }
+
+  context.putImageData(imageData, 0, 0);
+
+  requestAnimationFrame(render);
+};
+
+render();
